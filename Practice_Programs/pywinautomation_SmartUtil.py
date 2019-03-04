a87908297f781db9cd72cfa6f977371f2821dd64
Mohammed Iqbal <FU148MYQ@fime.com>
2019-03-04 20:11:50 +0530

    automation program(opening a application).

---
+++ Practice_Programs/pywinautomation_SmartUtil.py
@@ -0,0 +1,13 @@
+from pywinauto import application
+import time
+import subprocess
+import os
+
+os.startfile("C:\\Program Files (x86)\\Orange Business Service\\SmartUtil v1.4\\SmartUtil.exe")
+
+#os.system('Beyond Compare 2.exe')
+
+#subprocess.call('C:\\Users\\FU148MYQ\\Desktop\\SmartUtil v1.4.exe')
+
+#app = application.Application()
+#app.start("C:\\Users\\FU148MYQ\\Desktop\\SmartUtil v1.4.exe")
