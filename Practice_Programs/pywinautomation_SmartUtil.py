9fc86f98e493ebb301a476ab9a6a12b80bdf4ffe
Mohammed Iqbal <FU148MYQ@fime.com>
2019-03-05 20:25:27 +0530

    updated automation code

--- Practice_Programs/pywinautomation_SmartUtil.py
+++ Practice_Programs/pywinautomation_SmartUtil.py
@@ -1,13 +1,14 @@
 from pywinauto import application
+from pywinauto.application import Application
 import time
 import subprocess
 import os
 
-os.startfile("C:\\Program Files (x86)\\Orange Business Service\\SmartUtil v1.4\\SmartUtil.exe")
+#os.startfile("C:\\Program Files (x86)\\Orange Business Service\\SmartUtil v1.4\\SmartUtil.exe")
 
-#os.system('Beyond Compare 2.exe')
-
-#subprocess.call('C:\\Users\\FU148MYQ\\Desktop\\SmartUtil v1.4.exe')
-
-#app = application.Application()
-#app.start("C:\\Users\\FU148MYQ\\Desktop\\SmartUtil v1.4.exe")
+#os.startfile("C:\\Program Files (x86)\\TeamViewer\\TeamViewer.exe")
+#app = Application(backend="uia").start("C:\\Program Files (x86)\\Orange Business Service\\SmartUtil v1.4\\SmartUtil.exe")
+app = Application(backend="win32").start("C:\\Program Files (x86)\\TeamViewer\\TeamViewer.exe")
+val = app.Properties.child_window(title="edit", auto_id="1001", control_type="Edit")
+print(val)
+app.Properties.print_control_identifiers()
