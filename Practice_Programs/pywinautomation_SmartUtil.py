f323dd1c6fcdb5218d75aada3e5a8eb5ba5b1c2c
Iqbal <Mohammediqbal18@gmail.com>
2019-03-10 18:40:40 +0530

    Updated code to open a dialog box, format the input, and open the application and start filling the data.

--- Practice_Programs/pywinautomation_SmartUtil.py
+++ Practice_Programs/pywinautomation_SmartUtil.py
@@ -3,8 +3,14 @@ from pywinauto.application import Application
 import time
 import subprocess
 import os
-import PyPDF2
 import docx2txt
+import tkinter as tk
+from tkinter import simpledialog, messagebox
+
+#pip install pywinauto
+#pip install docx2txt
+#pip install tkintertable
+#pip install win32gui
 
 #os.startfile("C:\\Program Files (x86)\\Orange Business Service\\SmartUtil v1.4\\SmartUtil.exe")
 #os.startfile("C:\\Program Files (x86)\\TeamViewer\\TeamViewer.exe")
@@ -12,52 +18,46 @@ import docx2txt
 #app = Application(backend="win32").start("C:\\Program Files (x86)\\TeamViewer\\TeamViewer.exe")
 #val = app.Properties.testingchild_window(title="edit", auto_id="1001", control_type="Edit")
 
-#app = Application(backend="win32").start("C:\\Program Files (x86)\\Orange Business Service\\SmartUtil v1.4\\SmartUtil.exe")
-#val = app.Properties.child_window(title="edit", auto_id="txtGetPOResponse", control_type="TextBox")
-
-#code to enter password
-#time.sleep(3)
-#keyboard.send_keys('testing2')
-
 """time.sleep(5)
-keyboard.send_keys('testing')
-keyboard.send_keys("{TAB 1}")
-keyboard.send_keys('testing2')
-keyboard.send_keys("{TAB 1}")
-keyboard.send_keys('testing3')
-keyboard.send_keys("{TAB 1}")
-keyboard.send_keys('testing4')
-keyboard.send_keys("{TAB 1}")
-keyboard.send_keys('testing5')
-keyboard.send_keys("{TAB 1}")
 keyboard.send_keys('testing6')
-keyboard.send_keys("{TAB 2}")
+keyboard.send_keys("{TAB 1}")
 keyboard.send_keys("{ENTER}")"""
 
-#code to open a txt file read the content and store in list. (pip install docx2txt)
-'''f = open("C:\\Users\\FU148MYQ\\Downloads\\FGBBDATA-9-001.docx","r")
-content = f.read()
-Contant_Lst = []
-Contant_Lst = content.split("	")
-print(Contant_Lst)
-print(len(Contant_Lst))'''
+#code to open a txt file read the content and store in list.
+#f = open("C:\\Users\\moham\\Desktop\\FGBBDATA-9-001.docx","r")
+#content = f.read()
+#my_text = docx2txt.process("C:\\Users\\moham\\Desktop\\FGBBDATA-9-001 (1).txt")
 
-my_text = docx2txt.process("C:\\Users\\FU148MYQ\\Downloads\\FGBBDATA-9-001 (1).docx")
-#print(my_text)
-Contant_Lst = my_text.split("	")
-newlst = []
+application_window = tk.Tk()
+app = application.Application()
 
-while '' in Contant_Lst:
-    Contant_Lst.remove('')
+def Start_Automate():    
+    content = simpledialog.askstring("Input", "What is your first name?",parent=application_window)
+    if content != None:
+        MultiSpace = ["               ", "              ", "             ", "            ", "           ", "          ", "         ", "        ", "       ", "      ","     ","    ","   ","  "]
 
+        while "  " in content:
+            for i in MultiSpace:
+                content = content.replace(i,"\t")
+
+        content = content.replace("\n", " ")        
+        Contant_Lst = content.split("	")
+        print(Contant_Lst)
+        print(len(Contant_Lst))
+        app.start("C:\\Program Files (x86)\\Lenovo\\SHAREit\\Shareit.exe")
+        app.window()
+        Start_Automate()
+    else:
+        messagebox.showinfo("Information","Automation has stopped, Thank You")
+
+
+"""time.sleep(5)
+
+k=0
 for i in range(40):
-    newlst.append(Contant_Lst[i])
-
-k = 0
-for j in newlst:
-    keyboard.send_keys(newlst[k])
+    keyboard.send_keys(Contant_Lst[k])
     keyboard.send_keys("{TAB 1}")
     k += 1
+"""
 
-print(newlst)
-print(len(newlst))
+Start_Automate()
