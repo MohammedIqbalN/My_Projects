e5a666f4c18dbae6c6c0fbc1106a572ef6b46903
Mohammed Iqbal <FU148MYQ@fime.com>
2019-03-11 15:22:17 +0530

    modified the logging messages and added few more check points in code.

---
+++ Practice_Programs/Automate.bat
@@ -0,0 +1,3 @@
+@echo off
+python E:\Projects_Local\Python\Git\My_Projects\Practice_Programs\pywinautomation_SmartUtil.py %*
+pause
--- Practice_Programs/pywinautomation_SmartUtil.py
+++ Practice_Programs/pywinautomation_SmartUtil.py
@@ -6,33 +6,29 @@ import os
 import docx2txt
 import tkinter as tk
 from tkinter import simpledialog, messagebox
+import sys
 
 #pip install pywinauto
 #pip install docx2txt
 #pip install tkintertable
 #pip install win32gui
 
-#os.startfile("C:\\Program Files (x86)\\Orange Business Service\\SmartUtil v1.4\\SmartUtil.exe")
 #os.startfile("C:\\Program Files (x86)\\TeamViewer\\TeamViewer.exe")
 
 #app = Application(backend="win32").start("C:\\Program Files (x86)\\TeamViewer\\TeamViewer.exe")
 #val = app.Properties.testingchild_window(title="edit", auto_id="1001", control_type="Edit")
 
-"""time.sleep(5)
-keyboard.send_keys('testing6')
-keyboard.send_keys("{TAB 1}")
-keyboard.send_keys("{ENTER}")"""
-
 #code to open a txt file read the content and store in list.
 #f = open("C:\\Users\\moham\\Desktop\\FGBBDATA-9-001.docx","r")
 #content = f.read()
+
 #my_text = docx2txt.process("C:\\Users\\moham\\Desktop\\FGBBDATA-9-001 (1).txt")
 
 application_window = tk.Tk()
 app = application.Application()
 
 def Start_Automate():    
-    content = simpledialog.askstring("Input", "What is your first name?",parent=application_window)
+    content = simpledialog.askstring("Input", "Enter you Data here",parent=application_window)
     if content != None:
         MultiSpace = ["               ", "              ", "             ", "            ", "           ", "          ", "         ", "        ", "       ", "      ","     ","    ","   ","  "]
 
@@ -40,15 +36,27 @@ def Start_Automate():
             for i in MultiSpace:
                 content = content.replace(i,"\t")
 
+        while " " in content:
+            content = content.replace(" ","{SPACE}") 
+
         content = content.replace("\n", " ")        
         Contant_Lst = content.split("	")
-        print(Contant_Lst)
-        print(len(Contant_Lst))
-        app.start("C:\\Program Files (x86)\\Lenovo\\SHAREit\\Shareit.exe")
-        app.window()
+        
+        Contant_Lst = [x for x in Contant_Lst if x]
+
+        if len(Contant_Lst) != 40:
+            messagebox.showerror("Error","Please Check the data entered as the splitted data does not have 40 feilds")
+        else:       
+            app.start("C:\\Program Files (x86)\\Orange Business Service\\SmartUtil v1.4\\SmartUtil.exe")
+            app.window()
+            keyboard.send_keys(Contant_Lst[0])
+            keyboard.send_keys("{TAB 1}")
+            keyboard.send_keys(Contant_Lst[1])
+            keyboard.send_keys("{TAB 1}")
+
         Start_Automate()
     else:
-        messagebox.showinfo("Information","Automation has stopped, Thank You")
+        messagebox.showinfo("Information","Automation Tool has stopped, Thank You")
 
 
 """time.sleep(5)
