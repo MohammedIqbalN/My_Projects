2223b201c12a08bd67b4ee0e80e38e6fd34507d5
Mohammed Iqbal <FU148MYQ@fime.com>
2019-03-19 18:17:46 +0530

    code for cx_Freeze and .exe file.

---
+++ Practice_Programs/cx_freeze/ReadMe.txt
@@ -0,0 +1,3 @@
+pip install wheel
+pip install cx_Freeze-5.1.1-cp37-cp37m-win_amd64.whl
+python setup.py bdist_msi
---
+++ Practice_Programs/cx_freeze/Setup.py
@@ -0,0 +1,3 @@
+#setup.py
+from cx_Freeze import setup, Executable
+setup(name="AutoFill", version="1.0.0", description="", executables=[Executable("Arfath_working_Code.py")])
